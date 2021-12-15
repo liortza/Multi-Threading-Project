fd98c3051ca3a70d95cb46620155e798035d3b3f
liorza <liortza@post.bgu.ac.il>
2021-12-15 13:32:57 +0200

    nn

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -1,11 +1,17 @@
 package bgu.spl.mics.application;
 
+import bgu.spl.mics.application.objects.ConfrenceInformation;
+import bgu.spl.mics.application.objects.Student;
+
+import java.util.ArrayList;
+
 /** This is the Main class of Compute Resources Management System application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output a text file.
  */
 public class CRMSRunner {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<Student> Students= new ArrayList<>();
+        ArrayList<ConfrenceInformation> Conferences= new ArrayList<>();
     }
 }
